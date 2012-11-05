413ad1db112d49d01f323ee943b8b60d0b9c1ffe
xuluan <rolle.xu@gmail.com>
2012-11-06 04:59:49 +0800

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freemark::Application.config.secret_token = '0e7661fdb0ef22e67448956ecc64d0df005cd4b619313b581d8185f008090d02a60ad7176fa4832fc0d17ac4fafcdfc697bc689f663a24a359a88ce2eb269686'
